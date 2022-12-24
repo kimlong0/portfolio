e18b19134fcd242e2c444d2bf22a2489bce37b3c
kimlong0 <kimlong0@uw.edu>
2022-12-23 21:14:24 -0800

    navbar iteration

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,61 +1,32 @@
+import React, { useState } from 'react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
+
 function Navbar() {
-  return (
-    <div>
-        <nav class="bg-white dark:bg-gray-800  shadow ">
-            <div class="px-8 mx-auto max-w-7xl">
-                <div class="flex items-center justify-between h-16">
-                    <div class="w-full justify-between flex items-center">
-                        <a class="flex-shrink-0" href="/">
-                            <img class="w-8 h-8" src="/banana.svg" alt="Workflow"/>
-                        </a>
-                        <div class="hidden md:block">
-                            <div class="flex items-baseline ml-10 space-x-4">
-                                <a class="text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#">
-                                    Home
-                                </a>
-                                <a class="text-gray-800 dark:text-white  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#">
-                                    Gallery
-                                </a>
-                                <a class="text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#">
-                                    Content
-                                </a>
-                                <a class="text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#">
-                                    Contact
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="block">
-                        <div class="flex items-center ml-4 md:ml-6">
-                        </div>
-                    </div>
-                    <div class="flex -mr-2 md:hidden">
-                        <button class="text-gray-800 dark:text-white hover:text-gray-300 inline-flex items-center justify-center p-2 rounded-md focus:outline-none">
-                            <svg width="20" height="20" fill="currentColor" class="w-8 h-8" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z">
-                                </path>
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div class="md:hidden">
-                <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                    <a class="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" href="/#">
-                        Home
-                    </a>
-                    <a class="text-gray-800 dark:text-white block px-3 py-2 rounded-md text-base font-medium" href="/#">
-                        Gallery
-                    </a>
-                    <a class="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" href="/#">
-                        Content
-                    </a>
-                    <a class="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" href="/#">
-                        Contact
-                    </a>
-                </div>
-            </div>
-        </nav>
+    const Links =[
+      {name:"HOME",link:"/"},
+      {name:"PROJECTS",link:"/"},
+      {name:"ABOUT",link:"/"},
+    ];
+    const [open, setOpen]=useState(false);
+    const linkElements = Links.map((link)=>(
+        <li key={link.name} className='md:ml-8 md:my-0 my-7'>
+            <a href={link.link} className='text-gray-800 hover:text-gray-400 duration-500'>{link.name}</a>
+        </li>
+    ))
+  
+    return (
+    <div className='w-full fixed top-0 left-0'>
+      <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-8'>
+        <h1 className='font-bold text-2xl cursor-pointer text-gray-800'>KN</h1>
+      
+        <button onClick={()=> setOpen(!open)} className='absolute right-8 top-5 cursor-pointer md:hidden'>
+            {open ? <XMarkIcon className='w-8'/> : <Bars3Icon className='w-8'/>}
+        </button>
+
+        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-8 transition-all duration-500 ease-in ${open ? 'top-12 ':'top-[-490px]'}`}>
+            {linkElements}
+        </ul>
+      </div>
     </div>
   )
 }
