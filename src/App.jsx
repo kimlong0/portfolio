3a7f20adc33d0f1df263567c741b337a9771982a
kimlong0 <kimlong0@uw.edu>
2022-12-26 01:52:04 -0800

    add gradient to header

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,15 @@ import Intro from "./Intro"
 
 function About() {
   return (
-    <div className="h-72 flex items-center py-8 px-9 bg-black text-white" id="About">
-      I am a Finance and Information Systems student at the University of Washington. Learning is my passion!
+    <div className="h-80 flex items-center py-8 px-9 bg-black" id="About">
+      <div className="flex flex-col">
+        <h3 className="text-xl font-bold text-white">
+          I am a Finance and Information Systems student at the University of Washington.
+        </h3>
+        <div className="bg-gradient-to-r from-[#FDD819] to-blue-400 bg-clip-text bg-400% animation-bg-animation">
+          <p className="text-2xl font-bold text-transparent">Learning is my passion</p>
+        </div>
+      </div>
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,18 @@ module.exports = {
         'gray-theme': '#F4F2EE',
         'orange-theme': '#FD8E58',
         'cyan-theme': '#7FD1AE'
+      },
+      backgroundSize: {
+        '400%': '400%'
+      },
+      animation: {
+        'bg-animation': 'bg-animation 1s infinite'
+      },
+      keyframes: {
+        'bg-animation': {
+          '0%, 100%': {'transform': 'translate-x-0'},
+          '50%': {'transform': 'translate-x-full'}
+        }
       }
     },
   },
