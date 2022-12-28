671f5dbc1fef2fd942b761fc11ec5345427c5644
kimlong0 <kimlong0@uw.edu>
2022-12-27 23:56:48 -0800

    margin mods

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Intro from "./Intro"
 
 function About() {
   return (
-    <div className="h-80 flex items-center py-8 px-9 bg-black" id="About">
+    <div className="h-1/2 flex flex-col md:flex-row justify-between items-center py-8 px-9 bg-black" id="About">
       <div className="flex flex-col">
         <h3 className="text-xl font-bold text-white">
           I am a Finance and Information Systems student at the University of Washington.
@@ -12,6 +12,7 @@ function About() {
           <p className="text-2xl font-bold text-transparent">Learning is my passion</p>
         </div>
       </div>
+      <img src="images/Profile-Pic.jpg" className="h-48 w-48 my-8 rounded-full object-cover"></img>
     </div>
   )
 }
@@ -19,9 +20,11 @@ function About() {
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Intro />
-      <About />
+      <div className="wrapper h-full">
+        <Navbar />
+        <Intro />
+        <About />
+      </div>
     </div>
   )
 }
