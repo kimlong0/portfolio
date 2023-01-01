e79ec7bd56a40166b0495fc02e49e3c832dd7779
kimlong0 <kimlong0@uw.edu>
2023-01-01 15:56:28 -0800

    add icons and moving components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
 import { useEffect, useState } from "react"
-import Navbar from "./Navbar"
+import Navbar from "./components/Navbar"
 import Intro from "./Intro"
-
-function ProjectCard(props) {
-  const project = props.project;
-  console.log(project);
-  return (
-    <div className="flex flex-col md:flex-row items-center w-4/5 h-128 p-6 md:p-16 max-w-screen-md bg-gray-200 rounded-4xl">
-      <a href={project.link} target="_blank" className="shrink-0">
-        <img src={`/images${project.img}`} className="h-72 w-72 md:h-96 md:w-80 object-cover rounded-3xl"></img>
-      </a>
-      <div className="flex flex-col self-start md:ml-10">
-        <h3 className="text-2xl font-bold py-6">Discover PNW</h3>
-        <a href={project.link} target="_blank">WEBSITE</a>
-      </div>
-    </div>
-  )
-}
-
-function Projects(props) {
-  const projects = props.projects;
-  const projectList = projects.map((project) => <ProjectCard key={project.name} project={project}/>)
-
-  return (
-    <div className="py-8 px-9 flex flex-col items-center">
-      <h2 className="text-2xl pb-8 font-bold bg-gradient-to-r from-[#7f87ff] to-[#ec3d43] bg-clip-text text-transparent">Some of my Projects</h2>
-      {projectList}
-    </div>
-  )
-}
+import ProjectsList from "./components/ProjectsList"
 
 function About() {
   return (
@@ -66,7 +39,7 @@ function App() {
       <Navbar />
       <Intro />
       <About />
-      <Projects projects={projects}/>
+      <ProjectsList projects={projects}/>
     </div>
   )
 }
