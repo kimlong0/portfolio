266931b09e971549c4262cf800a6fc3e1f26249d
kimlong0 <kimlong0@uw.edu>
2023-01-01 22:39:53 -0800

    changing logo names and displaying tools

--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -1,22 +1,26 @@
 function ProjectCard(props) {
     const project = props.project;
     console.log(project);
+
+    const toolsList = project.tools.map((tool) => {
+        return (
+            <img key={tool} width="24" src={`/images/icons/${tool}.svg`} alt={tool} className="ml-2"/>
+        )
+    });
+
     return (
-      <div className="flex flex-col md:flex-row items-center w-4/5 h-128 p-6 md:p-16 max-w-screen-md bg-gray-300 rounded-4xl">
+      <div className="flex flex-col md:flex-row items-center w-4/5 h-128 p-6 md:p-16 max-w-screen-md bg-gray-900 text-white rounded-4xl">
         <a href={project.link} target="_blank" className="shrink-0">
           <img src={`/images${project.img}`} className="h-72 w-72 md:h-96 md:w-80 object-cover rounded-3xl" alt={project.name + " Cover"}></img>
         </a>
         <div className="flex flex-col self-start md:ml-10">
           <h3 className="text-2xl font-bold py-6">Discover PNW</h3>
           <a href={project.link} target="_blank">WEBSITE</a>
-          <div>
-            <p>Code</p>
-            <img width="24" src="/images/icons/github-mark.svg" alt="Github"/>
-          </div>
-          <div>
-            <p>Tools</p>
-            <img width="24" src="/images/icons/Firebase_Logo_Logomark.svg" alt="Firebase"/>
-            <img width="24" src="/images/icons/react-logo.svg" alt="React"/>
+          <div className="flex md:flex-col py-3">
+            <p className="py-1">TOOLS</p>
+            <div className="flex flex-wrap">
+                {toolsList}
+            </div>
           </div>
         </div>
       </div>
