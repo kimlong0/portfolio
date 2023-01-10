931ee794639e57f0f17478768c8cc5cd06de7a7c
kimlong0 <kimlong0@uw.edu>
2023-01-09 21:20:08 -0800

    add hover effects on cards

--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -13,15 +13,16 @@ function ProjectCard(props) {
     const logo = project.img ? <img src={`/images/${project.img}`} className="w-60"/> : <h2 className="text-3xl font-medium">{project.name}</h2>
 
     return (
-      <div className="sm:basis-1/2 p-1 w-full">
-        <a href={project.link} target="_blank">
+      <div className="sm:basis-1/2 p-1 w-full hover:scale-95 ease-in duration-100">
+        <a href={project.link} target="_blank" className="">
           <div className="relative flex flex-col items-center justify-center h-128 w-full p-6 sm:p-16 text-black rounded-5xl z-[-1]" style={{backgroundColor: project.color}}>
             <div className="absolute top-0 mt-8 flex flex-wrap gap-1">
               {toolsList}
             </div>
+            
+            {logo}
             <ArrowDownRightIcon width="25" className="absolute bottom-0 right-8 mb-8"/>
             <CodeBracketIcon width="25" className="absolute bottom-0 mb-8"/>
-            {logo}
           </div>
         </a>
       </div>
