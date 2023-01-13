68ca97bf27b9d216dcf4587233db6f03dc12ca24
kimlong0 <kimlong0@uw.edu>
2023-01-13 00:15:16 -0800

    project card adjustments

--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -5,25 +5,31 @@ function ProjectCard(props) {
 
   const toolsList = project.tools.map((tool) => {
       return (
-          <img key={tool} width="24" src={`/images/icons/${tool}.svg`} alt={tool} className="mb-8"/>
+          <img key={tool} width="24" src={`/images/icons/${tool}.svg`} alt={tool}/>
       )
   });
   
   // If project has no logo image then the logo is the project's name.
-  const logo = project.img ? <img src={`/images/${project.img}`} className="w-60"/> : <h2 className="text-3xl text-center font-medium">{project.name}</h2>
+  const logo = project.img ? <img src={`/images/${project.img}`} className="w-60 rounded-4xl"/> : <h2 className="text-3xl text-center font-medium">{project.name}</h2>
   
   return (
     <div className="sm:basis-1/2 p-1 w-full hover:scale-95 hover:brightness-110 ease-in duration-200">
-      <a href={project.link} target="_blank" className="">
         <div className="relative flex flex-col items-center justify-center h-96 w-full p-6 sm:p-16 text-black rounded-5xl z-[-1]" style={{backgroundColor: project.color}}>
-          <div className="absolute top-0 mt-10 flex flex-wrap gap-1">
+          <a href={project.source} target="_blank">
+            <CodeBracketIcon width="25" className="absolute top-10 right-10"/>
+          </a>
+          <a href={project.link} target="_blank">
+            <div className="rounded-4xl shadow-2xl hover:border-black hover:border-2 duration-300 ease-in">
+              {logo}
+            </div>
+          </a>
+          <div className="absolute bottom-0 left-10 mb-10 flex flex-wrap gap-1">
             {toolsList}
           </div>
-          {logo}
-          <ArrowDownRightIcon width="25" className="absolute bottom-0 right-10 mb-10"/>
-          <CodeBracketIcon width="25" className="absolute bottom-0 mb-10"/>
+          <a href={project.link} target="_blank">
+            <ArrowDownRightIcon width="25" className="absolute bottom-0 right-10 mb-10"/>
+          </a>
         </div>
-      </a>
     </div>
 
   )
