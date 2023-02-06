4d11e481648fe2e81d9226ed20a0a070de4627d0
kimlong0 <kimlong0@uw.edu>
2023-02-05 17:44:38 -0800

    starter parallax on intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,8 +1,10 @@
 import { motion, useScroll, useTransform } from "framer-motion"
 
-function Intro(props) {
+function Intro() {
   let { scrollYProgress } = useScroll();
-  let x = useTransform(scrollYProgress, [0, 0.5], [1000,  -100]);
+  let x = useTransform(scrollYProgress, [0.18, 0.6], ["50%",  "-500%"]);
+  let y = useTransform(scrollYProgress, [0, 1], ["0%", "400%"]);
+  let opacity = useTransform(scrollYProgress, [0, 0.25, 0.3], [ 1, 1, 0]);
   // const transformValue = props.transformValue;
   // console.log(transformValue);
 
@@ -12,9 +14,10 @@ function Intro(props) {
         initial={{ y: -250 }} 
         animate={{ y: 0 }}
         transition={{ duration: 1.5 }}
-        className="bg-gray-theme rounded-b-5xl">
+        className="bg-gray-theme rounded-b-5xl relative z-[-1]"
+        style={{ y, opacity }}>
         <div className="h-20"></div>
-        <div className="flex flex-col md:flex-row justify-center items-center p-4 mb-1">
+        <div className="flex flex-col md:flex-row h-[calc(100vh_-_15rem)] justify-center items-center p-4 mb-1">
             <h1 className="text-4xl md:text-6xl font-bold text-center py-8 px-9 bg-gradient-to-r from-[#2ca2b4] via-[#7f87ff] to-[#ec3d43] bg-clip-text text-transparent">Hello World</h1>
             <div className="max-w-xl">
                 <img src="images/paint-explosion.png" className="rounded-3xl"></img>
@@ -22,8 +25,8 @@ function Intro(props) {
         </div>
       </motion.div>
 
-      <div className="flex items-center h-screen bg-orange-400 rounded-5xl">
-        <motion.h1 className="text-4xl md:text-6xl font-bold" style={{ x }} >Let's Explore With Me</motion.h1>
+      <div className="flex items-center justify-end h-screen bg-lime-200 rounded-5xl border-2 border-black">
+        <motion.h1 className="text-4xl md:text-6xl font-bold" style={{ x }}>Explore With Me</motion.h1>
       </div>
     </div>
   )
