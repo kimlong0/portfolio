2540d762e79eb0ac75ea1cd2aa33272ea66b481f
kimlong0 <kimlong0@uw.edu>
2023-02-08 13:05:41 -0800

    add loading page before initial state load

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { useEffect, useState } from "react"
-import { motion, useScroll, useTransform } from "framer-motion"
+import { motion } from "framer-motion"
+
+// Components
 import Navbar from "./components/Navbar"
 import Intro from "./components/Intro"
 import About from "./components/About"
 import ProjectsList from "./components/ProjectsList"
 import ConnectWithMe from "./components/ConnectWithMe"
 import Footer from "./components/Footer"
+import Loading from "./assets/Loading"
 
 function App() {
   const [projects, setProjects] = useState([]);
-
-  let { scrollYProgress } = useScroll();
-  let x = useTransform(scrollYProgress, [0, 0.5], [100,  -100]);
+  const [isQuerying, setIsQuerying] = useState(false);
 
   useEffect(() => {
+    // Set loading state
+    setIsQuerying(true);
     fetch('/data/projects.json').catch((error) => {
       console.log(error);
     })
@@ -23,24 +26,40 @@ function App() {
     .then((data) => {
       setProjects(data);
 
-      // On page refresh/data-load, scroll to the top of the page. 
-      window.scrollTo(0, 0);
+    // Disable loading state
+    setIsQuerying(false);
+
+    // On page refresh/data-load, scroll to the top of the page. 
+    window.scrollTo(0, 0);
     })
   }, []);
 
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1 }}
-    >
-      <Navbar />
-      <Intro />
-      <About />
-      <ProjectsList projects={projects}/>
-      <ConnectWithMe />
-      <Footer />
-    </motion.div>
+    <div>
+      {isQuerying && <LoadingIconWrapper />}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <Navbar />
+        <main>
+          <Intro />
+          <About />
+          <ProjectsList projects={projects}/>
+          <ConnectWithMe />
+          <Footer />
+        </main>
+      </motion.div>
+    </div>
+  )
+}
+
+function LoadingIconWrapper() {
+  return (
+    <div className='flex justify-center items-center h-screen bg-gray-theme'>
+      <Loading />
+    </div>
   )
 }
 
